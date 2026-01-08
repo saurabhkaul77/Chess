15c01303513ee73e54d80c012d734f73912171dc
saurabhkaul77 <77skaul@gmail.com>
2026-01-08 20:16:03 +0530

    backend done

--- app.js
+++ app.js
@@ -24,6 +24,47 @@ app.get("/", (req, res)=>{
 
 io.on("connection", function(uniquesocket){
     console.log("Connected");
+
+    if(!player.white){
+        player.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "w");
+    }
+    else if(!player.black){
+        player.white = uniquesocket.id;
+        uniquesocket.emit("playerRole", "b");
+    }
+    else{
+        uniquesocket.emit("spectatorRole")
+    }
+
+    uniquesocket.on("disconnect", function(){
+        if(uniquesocket.id === player.white){
+            delete player.white;
+        }else if(uniquesocket.id === player.black){
+            delete player.black
+        }
+    })
+
+    uniquesocket.on("move", (move)=>{
+        try {
+            if(chess.turn() === 'w' && uniquesocket.id !== player.white) return;
+            if(chess.turn() === 'b' && uniquesocket.id !== player.black) return;
+
+            const result = chess.move(move)
+            if(result){
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen())
+            }else{
+                console.log("Invalid move: ", move);
+                uniquesocket.emit("invalidMove", move)                
+            }
+        } catch (err) {
+            console.log(err);        
+            uniquesocket.emit("Invalid move: ",move);            
+        }
+    })
+
 })
 
 server.listen(3000, function(){
